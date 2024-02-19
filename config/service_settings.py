96d5d55721a0e45697a16444689e2d6e47f8cec6
alex-ib <alejo1ibarra@gmail.com>
2024-02-18 20:05:50 -0500

    :sparkles: create base configuration of the project

---
+++ config/service_settings.py
@@ -0,0 +1,28 @@
+import os
+from distutils.util import strtobool
+
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
+VERSION: str = "0.1.0"
+
+INSTALLED_APPS: list[str] = [
+    "users",
+]
+
+DEBUG: bool = bool(strtobool(os.environ.get("DEBUG", "False")))
+
+# CORS
+CORS_ALLOWED_METHODS: list[str] = os.environ.get("CORS_ALLOWED_METHODS", "*").split(",")
+CORS_ALLOWED_HEADERS: list[str] = os.environ.get("CORS_ALLOWED_METHODS", "*").split(",")
+CORS_ALLOWED_HOSTS: list[str] = os.environ.get("CORS_ALLOWED_HOSTS", "").split(",")
+
+
+# Database
+DB_NAME: str = os.environ.get("DB_NAME")
+DB_USER: str = os.environ.get("DB_USER")
+DB_PORT: str = os.environ.get("DB_PORT", "5432")
+DB_PASSWORD: str = os.environ.get("DB_PASSWORD")
