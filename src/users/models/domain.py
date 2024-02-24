a0d4aae79c33e3e2e19fc0ca21102acbfc7eb223
alex-ib <alejo1ibarra@gmail.com>
2024-02-24 15:50:35 -0500

    :art: update user attr typing

--- src/users/models/domain.py
+++ src/users/models/domain.py
@@ -1,11 +1,12 @@
-from datetime import datetime
+from uuid import UUID
+from datetime import date
 
 
 class User:
-    def __init__(self, uid: int, name: str, age: int, birthdate: datetime, email: str, hashed_password: str):
-        self.uid: str = uid
+    def __init__(self, uid: int, name: str, age: int, birthdate: date, email: str, hashed_password: str):
+        self.uid: UUID = uid
         self.name: str = name
         self.age: int = age
-        self.birthdate: datetime = birthdate
+        self.birthdate: date = birthdate
         self.email: str = email
         self.hashed_password: str = hashed_password
